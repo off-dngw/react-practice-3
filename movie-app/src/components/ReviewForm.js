1fc26e40c2df352d65d46f48919b8cdc9291bead
off-dngw <rlaehddn0729@gmail.com>
2022-05-14 21:33:36 +0900

    .

--- movie-app/src/components/ReviewForm.js
+++ movie-app/src/components/ReviewForm.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 // import Rating from "./Rating";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
+
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
 
 function sanitize(type, value) {
   switch (type) {
@@ -14,16 +22,13 @@ function sanitize(type, value) {
   }
 }
 
-const ReviewForm = () => {
+const ReviewForm = ({ onSubmitSuccess }) => {
   //   const [title, setTitle] = useState("");
   //   const [rating, setRating] = useState(0);
   //   const [content, setContent] = useState("");
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
 
   const handleChage = (name, value, type) => {
     setValues((prevValues) => ({
@@ -53,8 +58,28 @@ const ReviewForm = () => {
   //     setContent(e.target.value);
   //   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+
+    let result;
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      result = await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+    const { review } = result;
+    onSubmitSuccess(review);
+    setValues(INITIAL_VALUES);
     console.log(values);
     // console.log({ title, rating, content });
   };
@@ -74,7 +99,10 @@ const ReviewForm = () => {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
 };
