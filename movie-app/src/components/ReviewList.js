db63183e2708fb83d1bb1398481fed4c0d4f5e55
off-dngw <rlaehddn0729@gmail.com>
2022-05-15 23:16:54 +0900

    Update ReviewList.js

--- movie-app/src/components/ReviewList.js
+++ movie-app/src/components/ReviewList.js
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState } from "react";
 import Rating from "./Rating";
+import ReviewForm from "./ReviewForm";
 import "./ReviewList.css";
 
 function formatDate(value) {
@@ -7,8 +8,15 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item, ondelete }) {
-  const handleDeleteClick = () => ondelete(item.id);
+function ReviewListItem({ item, onDelete, onEdit }) {
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  };
+
+  const handleEditClick = () => {
+    onEdit(item.id);
+  };
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -17,24 +25,46 @@ function ReviewListItem({ item, ondelete }) {
         <Rating value={item.rating} />
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
-        <button onClick={handleDeleteClick}>삭제하기</button>
+        <button onClick={handleEditClick}>수정</button>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-const ReviewList = ({ items, ondelete }) => {
+function ReviewList({ items, onDelete }) {
+  const [editingId, setEditingId] = useState(null);
+
+  const handleCancel = () => setEditingId(null);
+
   return (
     <ul>
-      {items.map((el) => {
+      {items.map((item) => {
+        if (item.id === editingId) {
+          const { imgUrl, title, rating, content } = item;
+          const initialValues = { title, rating, content, imgFile: null };
+          return (
+            <li key={item.id}>
+              <ReviewForm
+                initialValues={initialValues}
+                initialPreview={imgUrl}
+                onCancel={handleCancel}
+              />
+            </li>
+          );
+        }
         return (
-          <li key={el.id}>
-            <ReviewListItem item={el} ondelete={ondelete} />
+          <li key={item.id}>
+            <ReviewListItem
+              item={item}
+              onDelete={onDelete}
+              onEdit={setEditingId}
+            />
           </li>
         );
       })}
     </ul>
   );
-};
+}
 
 export default ReviewList;
