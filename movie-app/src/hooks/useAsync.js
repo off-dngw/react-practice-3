f1fe803987938bd2443a56d6d92e2546da879f90
off-dngw <rlaehddn0729@gmail.com>
2022-05-17 23:08:52 +0900

    Update useAsync.js

--- movie-app/src/hooks/useAsync.js
+++ movie-app/src/hooks/useAsync.js
@@ -1,6 +1,6 @@
 // 커스텀훅 만들기
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function useAsync(asnyFunction) {
   const [pending, setPending] = useState(false);
@@ -8,8 +8,9 @@ function useAsync(asnyFunction) {
 
   // 네트워크 req 보낼때 사용할 함수
 
-  const wrappedFunction = async (...args) => {
-    /*
+  const wrappedFunction = useCallback(
+    async (...args) => {
+      /*
     try catch 문에서 비동기 처리를 하면서 asyncFunction 함수를 실행 시킵니다.
     asyncFunction은 기존에 우리가 사용하던 api 함수에 해당된다고 생각하시면 됩니다.
     asyncFunction은 실제로 req를 보내는 함수가 됩니다.
@@ -18,16 +19,18 @@ function useAsync(asnyFunction) {
     wrappedFunction 에서는 로딩과 에러처리를 해주기 때문에 우리가 따로 처리해주지 않아도 되는 장점이 있습니다
     */
 
-    try {
-      setError(null);
-      setPending(true);
-      return await asnyFunction(...args);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setPending(false);
-    }
-  };
+      try {
+        setError(null);
+        setPending(true);
+        return await asnyFunction(...args);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setPending(false);
+      }
+    },
+    [asnyFunction]
+  );
   return [pending, error, wrappedFunction];
 }
 
